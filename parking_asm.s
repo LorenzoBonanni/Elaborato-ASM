589ee58d06f880258b95699e88f1523b64847522
LorenzoBonanni <lorenzo.bonanni@studenti.univr.it>
2020-07-25 16:18:33 +0200

    modified a labels, created b, created c and added '\n' check into day

--- parking_asm.s
+++ parking_asm.s
@@ -19,23 +19,30 @@ count: .long 0
 
 .section .text # sezione istruzioni
 .global day 
-    
+    add_one:
+        addl $1, %edx
+        jmp on
+
     day:
         movl 4(%esp), %esi
         movl 8(%esp), %edi
         movl $0, %edx
-        xorl %ebx, %ebx
         on:
+        xorl %ebx, %ebx
         # se count è tre ho settato il valore iniziale di tutti i parcheggi
         cmp $3, count
         je normale
         # metto in bl il carattere presente a uno spiazzamento edx di esi
         movb (%edx, %esi, 1), %bl
+        
+        # check if bl value is \n
+        cmp lett_acapo, %bl
+        je add_one
 
         cmp lett_b, %bl
         jg c    # se carattere maggiore di b allora sarà c
         jl a    # se carattere minore di b allora sarà a 
-        je b
+        jmp b
 
     a:
         inc %edx
@@ -44,15 +51,15 @@ count: .long 0
         je a    # bl == '-' -> torna ad a
 
         cmp lett_acapo, %bl
-        je single_number # bl == '\n' -> vai a single_number
+        je single_number_a # bl == '\n' -> vai a single_number
 
         subb $48, %bl   # trasforma il valore di bl da carattere ascii a numero
         cmp $0, %bh
-        jne continue
+        jne continue_a
         movb %bl, %bh
         jmp a
 
-        continue:
+        continue_a:
         # ax = bl*10
         movb $10, %al
         mulb %bh
@@ -60,14 +67,56 @@ count: .long 0
         # insert in A the number of cars if the value is composed by 2numbers
         xorb %bh, %bh
         addw %ax, %bx
-        movw %ax, A
-        jmp end
+        movw %bx, A
+        jmp end_a
 
         # insert in A the number of cars if the value is composed by 1number
-        single_number:
+        single_number_a:
         movb %bh, A
 
-        end:
+        end_a:
+        # increase count
+        xorl %eax, %eax
+        movl count, %eax
+        inc %eax
+        movl %eax, count
+
+        #increase edx and retrurn to on
+        inc %edx
+        jmp on
+    
+
+    b:
+        inc %edx
+        movb (%edx, %esi, 1), %bl
+        cmp trattino, %bl
+        je b    # bl == '-' -> jump to b
+
+        cmp lett_acapo, %bl
+        je single_number_b # bl == '\n' -> jump to single_number
+
+        subb $48, %bl   # transform bl from ascii to number
+        cmp $0, %bh
+        jne continue_b  # if bh is 0 it means that the value of bh is the first of 2 number or it is the only one
+        movb %bl, %bh
+        jmp b
+
+        continue_b:
+        # ax = bl*10
+        movb $10, %al
+        mulb %bh
+
+        # insert into B the number of cars if the value is composed by 2numbers
+        xorb %bh, %bh
+        addw %ax, %bx
+        movw %bx, B
+        jmp end_b
+
+        # insert into B the number of cars if the value is composed by 1number
+        single_number_b:
+        movb %bh, B
+
+        end_b:
         # increase count
         xorl %eax, %eax
         movl count, %eax
@@ -78,17 +127,46 @@ count: .long 0
         inc %edx
         jmp on
 
-
     c:
         inc %edx
         movb (%edx, %esi, 1), %bl
-        movb %cl, B
-    
+        cmp trattino, %bl
+        je c    # bl == '-' -> jump to b
 
-    b:
+        cmp lett_acapo, %bl
+        je single_number_c # bl == '\n' -> jump to single_number
+
+        subb $48, %bl   # transform bl from ascii to number
+        cmp $0, %bh
+        jne continue_c  # if bh is 0 it means that the value of bh is the first of 2 number or it is the only one
+        movb %bl, %bh
+        jmp c
+
+        continue_c:
+        # ax = bl*10
+        movb $10, %al
+        mulb %bh
+
+        # insert in C the number of cars if the value is composed by 2numbers
+        xorb %bh, %bh
+        addw %ax, %bx
+        movw %bx, C
+        jmp end_c
+
+        # insert in B the number of cars if the value is composed by 1number
+        single_number_c:
+        movb %bh, C
+
+        end_c:
+        # increase count
+        xorl %eax, %eax
+        movl count, %eax
+        inc %eax
+        movl %eax, count
+
+        #increase edx and retrurn to on
         inc %edx
-        movb (%edx, %esi, 1), %bl
-        movb %cl, B
-    
+        jmp on
+
     normale:
         xorl %eax, %eax
